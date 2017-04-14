f3857cf9e081b8a4473e1630f03203d017bee136
Everton miranda de oliveira <evt.miranda@outlook.com>
2017-04-14 11:37:46 -0300

    diversas inclusoes e melhorias

---
+++ marmitex-admin/Controllers/CardapioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace marmitex_admin.Controllers
+{
+    public class CardapioController : Controller
+    {
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Adicionar()
+        {
+            return View();
+        }
+    }
+}
---
+++ marmitex-admin/Controllers/HorarioEntregaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace marmitex_admin.Controllers
+{
+    public class HorarioEntregaController : Controller
+    {
+        // GET: HorarioEntrega
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
