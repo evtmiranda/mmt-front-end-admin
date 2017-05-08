20429efb3a6cc0c4bf7ff247c204fc7e06f4edae
Everton miranda de oliveira <evt.miranda@outlook.com>
2017-05-08 20:30:19 -0300

    conclusao da logica de parceiros

--- marmitex-admin/Controllers/ParceiroController.cs
+++ marmitex-admin/Controllers/ParceiroController.cs
@@ -13,18 +13,19 @@ namespace marmitex_admin.Controllers
     {
         private RequisicoesREST rest;
         private DadosRequisicaoRest retornoRequest;
-        private List<Parceiro> listaParceiros;
+
 
         //O Ninject é o responsável por cuidar da criação de todos esses objetos
         public ParceiroController(RequisicoesREST rest)
         {
             this.rest = rest;
-            this.listaParceiros = new List<Parceiro>();
         }
 
         // GET: Parceiro
         public ActionResult Index()
         {
+            List<Parceiro> listaParceiros = new List<Parceiro>();
+
             //se a sessão de usuário não estiver preenchida, direciona para a tela de login
             if (Session["UsuarioLogado"] == null)
             {
@@ -56,6 +57,9 @@ namespace marmitex_admin.Controllers
             string jsonParceiros = retornoRequest.objeto.ToString();
 
             listaParceiros = JsonConvert.DeserializeObject<List<Parceiro>>(jsonParceiros);
+
+            //mantém somente os parceiros ativos
+            listaParceiros = listaParceiros.Where(p => p.Ativo).ToList();
 
             return View(listaParceiros);
         }
@@ -92,7 +96,7 @@ namespace marmitex_admin.Controllers
             string dominioLoja = Session["dominioLoja"].ToString();
 
             DadosRequisicaoRest retornoRequest = new DadosRequisicaoRest();
-           
+
 
             try
             {
@@ -140,6 +144,169 @@ namespace marmitex_admin.Controllers
             {
                 ViewBag.MensagemParceiro = "não foi possível cadastrar o parceiro. por favor, tente novamente";
                 return View("Adicionar", parceiroCadastro);
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            //se a sessão de usuário não estiver preenchida, direciona para a tela de login
+            if (Session["UsuarioLogado"] == null)
+            {
+                Session["MensagemAutenticacao"] = "estamos com dificuldade em buscar dados no servidor. por favor, tente novamente";
+                return RedirectToAction("Index", "Login");
+            }
+
+            //recebe o usuário logado
+            usuarioLogado = (UsuarioLoja)(Session["UsuarioLogado"]);
+            usuarioLogado.UrlLoja = BuscarUrlLoja();
+
+            //busca os dados do parceiro
+            Parceiro parceiro = new Parceiro();
+
+            //busca o parceiro pelo id
+            retornoRequest = rest.Get("/Parceiro/BuscarParceiro/" + id);
+
+            //se não encontrar um parceiro com este id
+            if (retornoRequest.HttpStatusCode == HttpStatusCode.NoContent)
+            {
+                ViewBag.MensagemEditarParceiro = "não foi possível carregar os dados do parceiro. por favor, tente atualizar a página ou entre em contato com o administrador do sistema...";
+                return View();
+            }
+
+            //se ocorrer algum erro
+            if (retornoRequest.HttpStatusCode != HttpStatusCode.OK)
+            {
+                ViewBag.MensagemEditarParceiro = "não foi possível carregar os dados do parceiro. por favor, tente atualizar a página ou entre em contato com o administrador do sistema...";
+                return View();
+            }
+
+            string jsonParceiro = retornoRequest.objeto.ToString();
+
+            parceiro = JsonConvert.DeserializeObject<Parceiro>(jsonParceiro);
+
+            ParceiroCadastro parceiroCadastro = new ParceiroCadastro
+            {
+                Id = parceiro.Id,
+                Nome = parceiro.Nome,
+                Descricao = parceiro.Descricao,
+                Ativo = parceiro.Ativo,
+                IdEndereco = parceiro.Endereco.Id,
+                Cep = parceiro.Endereco.Cep,
+                UF = parceiro.Endereco.UF,
+                Cidade = parceiro.Endereco.Cidade,
+                Bairro = parceiro.Endereco.Bairro,
+                Logradouro = parceiro.Endereco.Logradouro,
+                NumeroEndereco = parceiro.Endereco.NumeroEndereco,
+                ComplementoEndereco = parceiro.Endereco.ComplementoEndereco
+            };
+
+            return View(parceiroCadastro);
+        }
+
+        [HttpPost]
+        public ActionResult EditarParceiro(ParceiroCadastro parceiroCadastro)
+        {
+            //captura a loja em questão
+            Session["dominioLoja"] = BuscarUrlLoja();
+
+            //se não conseguir capturar a loja, direciona para a tela de erro
+            if (Session["dominioLoja"] == null)
+            {
+                Session["MensagemAutenticacao"] = "estamos com dificuldade em buscar dados no servidor. por favor, tente atualizar a página";
+                return View("Index");
+            }
+
+            string dominioLoja = Session["dominioLoja"].ToString();
+
+            //variável para armazenar o retorno da requisição
+            DadosRequisicaoRest retornoRequest = new DadosRequisicaoRest();
+
+            try
+            {
+
+                Parceiro parceiro = new Parceiro()
+                {
+
+                    //cria um parceiro com os dados vindos da tela
+                    Id = parceiroCadastro.Id,
+                    Nome = parceiroCadastro.Nome,
+                    Descricao = parceiroCadastro.Descricao,
+                    Ativo = parceiroCadastro.Ativo,
+                    Endereco = new Endereco
+                    {
+                        Id = parceiroCadastro.IdEndereco,
+                        Cep = parceiroCadastro.Cep,
+                        UF = parceiroCadastro.UF,
+                        Cidade = parceiroCadastro.Cidade,
+                        Bairro = parceiroCadastro.Bairro,
+                        Logradouro = parceiroCadastro.Logradouro,
+                        NumeroEndereco = parceiroCadastro.NumeroEndereco,
+                        ComplementoEndereco = parceiroCadastro.ComplementoEndereco
+                    }
+                };
+
+                string urlPost = string.Format("/Parceiro/Atualizar");
+
+                retornoRequest = rest.Post(urlPost, parceiro);
+
+                //se o parceiro não for atualizado
+                if (retornoRequest.HttpStatusCode != HttpStatusCode.OK)
+                {
+                    ViewBag.MensagemEditarParceiro = "não foi possível atualizar o parceiro. por favor, tente novamente";
+                    return View("Editar", parceiroCadastro);
+                }
+
+                //se o parceiro for atualizado, direciona para a tela de visualização de parceiros
+                return RedirectToAction("Index", "Parceiro");
+            }
+            catch (Exception)
+            {
+                ViewBag.MensagemParceiro = "não foi possível atualizar o parceiro. por favor, tente novamente";
+                return View("Editar", parceiroCadastro);
+            }
+        }
+
+        public ActionResult Excluir(int id)
+        {
+            try
+            {
+                //se a sessão de usuário não estiver preenchida, direciona para a tela de login
+                if (Session["UsuarioLogado"] == null)
+                {
+                    Session["MensagemAutenticacao"] = "estamos com dificuldade em buscar dados no servidor. por favor, tente novamente";
+                    return RedirectToAction("Index", "Login");
+                }
+
+                //recebe o usuário logado
+                usuarioLogado = (UsuarioLoja)(Session["UsuarioLogado"]);
+                usuarioLogado.UrlLoja = BuscarUrlLoja();
+
+                //busca os dados do parceiro
+                Parceiro parceiro = new Parceiro
+                {
+                    Id = id,
+                    Ativo = false
+                };
+
+                //inativa o parceiro
+                string urlPost = string.Format("/Parceiro/Excluir");
+
+                retornoRequest = rest.Post(urlPost, parceiro);
+
+                //se o parceiro não for atualizado
+                if (retornoRequest.HttpStatusCode != HttpStatusCode.OK)
+                {
+                    ViewBag.MensagemExcluirParceiro = "não foi possível excluir o parceiro. por favor, tente novamente";
+                    return View("Index");
+                }
+
+                //se o parceiro for inativado, direciona para a tela de visualização de parceiros
+                return RedirectToAction("Index", "Parceiro");
+            }
+            catch (Exception)
+            {
+                ViewBag.MensagemExcluirParceiro = "não foi possível excluir o parceiro. por favor, tente novamente";
+                return View("Index");
             }
         }
     }
